6df3839b98ae12764043f81ca3d9aab9c073fdb9
David Briscoe <idbrii@gmail.com>
2022-12-22 15:36:54 -0800

    Convert ui into classes
    I tried using _init to pass data directly and it created overlapping
    widgets in my panel. Going back to these create functions have the same
    problem. How do I make this work?

--- addons/editor-script-bar/ui.gd
+++ addons/editor-script-bar/ui.gd
@@ -1,33 +1,6 @@
 # Helper functions for creating widgets.
 
-
-static func float(label_text: String):
-    var w := HBoxContainer.new()
-    var label := Label.new()
-    label.text = label_text
-    w.add_child(label)
-    var spin := SpinBox.new()
-    w.add_child(spin)
-    return w
-
-
-
-static func vec3(label_text: String):
-    var w := HBoxContainer.new()
-    _append_float(w, "X")
-    _append_float(w, "Y")
-    _append_float(w, "Z")
-    return _inject_optional_label(w, label_text)
-
-
-static func vec2(label_text: String):
-    var w := HBoxContainer.new()
-    _append_float(w, "X")
-    _append_float(w, "Y")
-    return _inject_optional_label(w, label_text)
-
-
-static func _append_float(root: Control, label_text):
+static func _append_float(root: Control, label_text: String):
     var label := Label.new()
     label.text = label_text
     root.add_child(label)
@@ -36,13 +9,89 @@ static func _append_float(root: Control, label_text):
     return spin
 
 
-static func _inject_optional_label(w: Control, label_text: String):
-    if label_text:
-        w.name = label_text
-        var top := VBoxContainer.new()
+
+class CommonControl extends HBoxContainer:
+
+    func _ready():
+        pass
+
+    func add_label(label_text: String):
         var label := Label.new()
         label.text = label_text
-        top.add_child(label)
-        top.add_child(w)
-        w = top
+        add_child(label)
+        move_child(label, 0)
+        return self
+
+    func add_to(parent):
+        parent.add_child(self)
+        return self
+
+    static func _append_float(root: Control, label_text: String):
+        var label := Label.new()
+        label.text = label_text
+        root.add_child(label)
+        var spin := SpinBox.new()
+        root.add_child(spin)
+        return spin
+
+
+
+class Float extends CommonControl:
+    var val: SpinBox
+
+    func _ready():
+        ._ready()
+        val = SpinBox.new()
+        self.add_child(val)
+
+    func get_value():
+        var txt := val.get_line_edit().text
+        return float(txt)
+
+    static func create(label_text, parent):
+        var w := Float.new()
+        parent.add_child(w)
+        w.add_label(label_text)
+        return w
+
+
+
+class Vec2 extends CommonControl:
+    var x : SpinBox
+    var y : SpinBox
+
+    func _ready():
+        ._ready()
+        x = _append_float(self, "X")
+        y = _append_float(self, "Y")
+
+    static func create(label_text, parent):
+        var w := Vec2.new()
+        parent.add_child(w)
+        w.add_label(label_text)
+        return w
+
+
+class Vec3 extends Vec2:
+    var z : SpinBox
+
+    func _ready():
+        ._ready()
+        z = _append_float(self, "Z")
+
+    static func create(label_text, parent):
+        var w := Vec3.new()
+        parent.add_child(w)
+        w.add_label(label_text)
+        return w
+
+
+
+static func _inject_optional_label(w: Control, label_text: String):
+    var top := VBoxContainer.new()
+    var label := Label.new()
+    label.text = label_text
+    top.add_child(label)
+    top.add_child(w)
+    w = top
     return w
--- code/editor/align_to_grid.gd
+++ code/editor/align_to_grid.gd
@@ -3,20 +3,38 @@ extends Launchable
 
 const ui = preload("res://addons/editor-script-bar/ui.gd")
 
+#~ class Inputs extends Control:
+#~     var offset
+#~     var max_width
+#~     func _ready():
+#~         offset = ui.vec3("Offset")
+#~         root.add_child()
+#~         root.add_child(ui.float("Max Width"))
+#~         root.add_child(run_btn)
+
+
+class Data extends Control:
+    var offset #: ui.Vec3
+    var max_width #: ui.Float
+
+    func _ready():
+        var w := VBoxContainer.new()
+        offset = ui.Vec3.create("Offset", w)
+        max_width = ui.Float.create("Max Width", w)
+        add_child(w)
+
 
 static func create_ui(run_btn: Button) -> Control:
-    printt("create_ui from")
-    var root := VBoxContainer.new()
+    printt("create_ui from align_to_grid.gd")
 
-    root.add_child(ui.vec3("Offset"))
-    root.add_child(ui.float("Max Width"))
+    var root := Data.new()
     root.add_child(run_btn)
     return root
 
 
 static func run_script(ed: EditorInterface, ui_root: Control):
     # TODO: How do I get the data out of ui?
-    var max_width := 100
+    var max_width = ui_root.max_width.get_value()
     var offset := Vector3(1.0, 0, 1.0)
 
     var selection := ed.get_selection().get_selected_nodes()

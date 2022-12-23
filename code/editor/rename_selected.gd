18d3d99f3a65bcc530933ac05e0aeec8301df6e5
David Briscoe <idbrii@gmail.com>
2022-12-22 18:15:14 -0800

    Make rename_selected reuse the first name

--- code/editor/rename_selected.gd
+++ code/editor/rename_selected.gd
@@ -2,11 +2,13 @@ tool
 extends LaunchableScript
 
 
+# Mostly an example of a script without any UI.
 static func run_script(ed: EditorInterface, ui_root: Control):
     var selection := LaunchableScript.get_selection_or_children(ed)
     var i := 0
+    var prefix = selection[0].name.rstrip(" 0123456789")
     for item in selection:
-        var name := "Renamed %d" % i
+        var name := "%s %d" % [prefix, i]
         printt(item.name, "->", name)
         item.name = name
         i += 1

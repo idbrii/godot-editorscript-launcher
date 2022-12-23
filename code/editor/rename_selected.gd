1e708624d81f14181ea0f4253d4790f5046114c4
David Briscoe <idbrii@gmail.com>
2022-12-22 16:14:24 -0800

    Rename nodes in visual order

--- code/editor/rename_selected.gd
+++ code/editor/rename_selected.gd
@@ -5,8 +5,16 @@ extends Launchable
 static func run_script(ed: EditorInterface, ui_root: Control):
     printt("/code/godot/addons/editor-script-bar/code/editor/rename_selected.gd")
     var sel := ed.get_selection()
+    var selection := sel.get_selected_nodes()
+    # Sort to ensure results match order seen in editor.
+    selection.sort_custom(SortByChildIndex.new(), "cmp")
     var i := 0
-    for item in sel.get_selected_nodes():
-        item.name = "Renamed %d".format(i)
+    for item in selection:
+        item.name = "Renamed %d" % i
+        printt(item.name)
         i += 1
 
+
+class SortByChildIndex extends Object:
+    func cmp(a, b):
+        return a.get_index() < b.get_index()
